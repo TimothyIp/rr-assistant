a9e2e0b6c2c205daca32c370dae4bf5b6a7c8c89
Timothy Ip <timothy.i@roserocket.com>
2023-12-12 16:58:05 -0500

    top k to be 4

--- openai_asker.py
+++ openai_asker.py
@@ -220,7 +220,7 @@ def ask_openai(context: BoltContext, question) -> str:
             },
         ).as_retriever(search_type="mmr", search_kwargs={"k": 20})
         compressor = CohereRerank(
-            cohere_api_key=os.environ["COHERE_API_KEY"], user_agent="langchain"
+            top_n=4, cohere_api_key=os.environ["COHERE_API_KEY"], user_agent="langchain"
         )
         retriever = ContextualCompressionRetriever(
             base_compressor=compressor, base_retriever=base_retriever
