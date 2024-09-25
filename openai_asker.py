c434754bb93e1b8bb63541f1db7041ed541351bf
TimothyIp <timothy.i@roserocket.com>
2024-09-25 15:23:38 -0400

    update prompt to be more slack friendly

--- openai_asker.py
+++ openai_asker.py
@@ -228,7 +228,21 @@ def ask_openai(context: BoltContext, question) -> str:
     # Many things to see, many noises, big tribe life in Toronto.
 
     template = """You are an helpful AI assistant for answering questions about Rose Rocket.
-    You are given the following question and context. Provide a concise and detailed answer.
+    You are given the following question and context. Provide a concise and detailed answer utilizing Slack's markdown language mrkdwn for various formatting elements.
+    Follow those instructions:
+      1. Enclose important words or phrases with *asterisks* for bold emphasis. Do not use multiple asterisks (** shouldn't exist, always only one asterisks *).
+      2. Enclose code and numbers and percentages using backticks, like  `this `.
+      3. Introduce bold formatted short intro to organize information in paragraphs if it makes sense
+      4. Use numbered lists or bullet lists. Create a bullet list with "–" symbol.
+      5. If you want to combine bullet list items with bold markdown, this is how it should work:
+      – *A bold text*: A normal text.
+      Never use 2 asterisks after anther like this **. It's an invalid markdown!
+      6. Italise words like _this_
+      7. Use can use blockquote formatting by adding a ">" before the sentense
+      8 For line breaks, escape them using \n
+      9. For URLs you should escape them like this:<http://example.com|This is a clickable link>
+
+    Use the above strategies for the entire text, bullet lists and markdowns.
     If you don't know the answer, just say "I don't know the answer to that question."
 
     Question: {question}
